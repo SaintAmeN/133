3cf6b0b8620394c0de0f552f65354e077e5f553e
Paweł Recław <preclaw@purestorage.com>
2022-09-04 15:17:04 +0200

    Zadanie 6

---
+++ src/main/java/pl/sda/j133/domowe/Main_zbior_1_przyklad_6.java
@@ -0,0 +1,33 @@
+package pl.sda.j133.domowe;
+
+import java.util.Scanner;
+
+public class Main_zbior_1_przyklad_6 {
+//    6. Wczytuj liczby tak długo aż użytkownik poda 5 liczb większych od zera i potem wypisz największą i najmniejszą z
+//              nich uwzględniając podane liczby ujemne
+
+    public static void main(String[] args) {
+        // Przykład 1:
+        // 1 2 3 4 5
+        //
+        // Max: 5
+        // Min: 1
+        //
+        // Przykład 2:
+        // -5 1 -300 2 3 -19 0 0  4 -10 5
+        //
+        // Max: 5
+        // Min: -300
+
+        Scanner scanner = new Scanner(System.in);
+
+        // zadeklaruj licznik
+        //
+        // w pętli...
+        // przyjmujemy liczby
+        // jeśli liczba > 0
+        //      zwiększ licznik
+        // jeśli licznik >= 5
+        //      wyskocz z pętli
+    }
+}
