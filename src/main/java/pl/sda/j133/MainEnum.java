7278ed370439e466cee7089a19b438d75a2a5b1a
Paweł Recław <preclaw@purestorage.com>
2022-09-04 13:08:22 +0200

    Parsowanie w pętli

--- src/main/java/pl/sda/j133/MainEnum.java
+++ src/main/java/pl/sda/j133/MainEnum.java
@@ -4,25 +4,40 @@ import java.util.Scanner;
 
 public class MainEnum {
     public static void main(String[] args) {
-        BiletMiejski biletMiejski1 = BiletMiejski.NORMALNY;
-        BiletMiejski biletMiejski2 = BiletMiejski.NORMALNY;
-
-        System.out.println(biletMiejski1 == biletMiejski2); // true
+//        BiletMiejski biletMiejski1 = BiletMiejski.NORMALNY;
+//        BiletMiejski biletMiejski2 = BiletMiejski.NORMALNY;
+//
+//        System.out.println(biletMiejski1 == biletMiejski2); // true
 
         Scanner scanner = new Scanner(System.in);
 
-        // załadować tekst (jedno słowo) ze scannera
-        String zmienna = scanner.next(); // Paweł
-        zmienna = zmienna.toUpperCase();
+        // for          - kiedy wiemy ile będzie iteracji
+        // while        - kiedy pętla może nie wykonać się ani razu
+        // do_while     - kiedy pętla musi wykonać się chociaż 1 raz
 
-        // wypisz na ekran
-        System.out.println(zmienna);     // PAWEŁ
+        boolean czyUdaloSieParsowacBilet = false;
 
-        // Parsowanie
-        BiletMiejski zmiennaBilet = BiletMiejski.valueOf(zmienna);
+        do {
+            System.out.println("Podaj rodzaj biletu (ULGOWY_STUDENCKI, ULGOWY_SENIOR, NORMALNY...):");
+            // załadować tekst (jedno słowo) ze scannera
+            String zmienna = scanner.next(); // zmienna => Paweł
+            zmienna = zmienna.toUpperCase(); // PAWEŁ
 
-        System.out.println(zmiennaBilet);
-        System.out.println(zmiennaBilet.getCena());
+            // wypisz na ekran
+            System.out.println(zmienna);
+
+            try {
+                // Parsowanie
+                BiletMiejski zmiennaBilet = BiletMiejski.valueOf(zmienna);
+                czyUdaloSieParsowacBilet = true;
+                System.out.println(zmiennaBilet);
+                System.out.println(zmiennaBilet.getCena());
+            } catch (IllegalArgumentException iae) {
+                System.err.println("Niepoprawna komenda, podaj poprawną nazwę biletu.");
+                // wywoła się tylko w przypadku błędu
+                czyUdaloSieParsowacBilet = false;
+            }
+        } while (!czyUdaloSieParsowacBilet); // wykonuj kod dopóki nie udało się parsować bilet
     }
 }
 
