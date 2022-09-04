4c3e394aec757caf03cef0bc55711a06ef55e89f
Paweł Recław <preclaw@purestorage.com>
2022-09-04 12:07:40 +0200

    Enum zadanie

--- src/main/java/pl/sda/j133/MainEnum.java
+++ src/main/java/pl/sda/j133/MainEnum.java
@@ -1,10 +1,29 @@
 package pl.sda.j133;
 
+import java.util.Scanner;
+
 public class MainEnum {
     public static void main(String[] args) {
         BiletMiejski biletMiejski1 = BiletMiejski.NORMALNY;
         BiletMiejski biletMiejski2 = BiletMiejski.NORMALNY;
 
         System.out.println(biletMiejski1 == biletMiejski2); // true
+
+        Scanner scanner = new Scanner(System.in);
+
+        // załadować tekst (jedno słowo) ze scannera
+        String zmienna = scanner.next(); // Paweł
+        zmienna = zmienna.toUpperCase();
+
+        // wypisz na ekran
+        System.out.println(zmienna);     // PAWEŁ
+
+        // Parsowanie
+        BiletMiejski zmiennaBilet = BiletMiejski.valueOf(zmienna);
+
+        System.out.println(zmiennaBilet);
+        System.out.println(zmiennaBilet.getCena());
     }
 }
+
+//
