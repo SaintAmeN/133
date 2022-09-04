ab5aa377b63ba22fc120120c2088b2465b587278
Paweł Recław <preclaw@purestorage.com>
2022-09-04 14:06:49 +0200

    Samochod dodany

---
+++ src/main/java/pl/sda/j133/zadanie_samochod/Main.java
@@ -0,0 +1,11 @@
+package pl.sda.j133.zadanie_samochod;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Samochod samochod = new Samochod();
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ src/main/java/pl/sda/j133/zadanie_samochod/Samochod.java
@@ -0,0 +1,27 @@
+package pl.sda.j133.zadanie_samochod;
+
+public class Samochod {
+    private int predkosc;                   // 0
+    private boolean swiatlaWlaczone;
+
+    // przyspiesz():void
+    public void przyspiesz() {
+        if (predkosc >= 120) {
+            System.err.println("Nie możesz przyspieszyć!");
+        } else {
+//            predkosc = predkosc + 10;
+            predkosc += 10;
+            System.out.println("Przyspieszam do " + predkosc + " km/h");
+        }
+    }
+
+    // czySwiatlaWlaczone():boolean
+    public boolean czySwiatlaWlaczone() {
+        return swiatlaWlaczone;
+    }
+
+    // wlaczSwiatla():void
+    void wlaczSwiatla() {
+        swiatlaWlaczone = true;
+    }
+}
