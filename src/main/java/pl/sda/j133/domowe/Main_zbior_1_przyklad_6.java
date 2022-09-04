14ddbc4174068ad0216acbe16e58b8343e2a8a0d
Paweł Recław <preclaw@purestorage.com>
2022-09-04 15:32:19 +0200

    Petla almost done

--- src/main/java/pl/sda/j133/domowe/Main_zbior_1_przyklad_6.java
+++ src/main/java/pl/sda/j133/domowe/Main_zbior_1_przyklad_6.java
@@ -22,12 +22,44 @@ public class Main_zbior_1_przyklad_6 {
         Scanner scanner = new Scanner(System.in);
 
         // zadeklaruj licznik
+        int licznik = 0;
+
         //
         // w pętli...
-        // przyjmujemy liczby
-        // jeśli liczba > 0
-        //      zwiększ licznik
-        // jeśli licznik >= 5
-        //      wyskocz z pętli
+        // for          - kiedy wiemy ile będzie iteracji
+        // while        - kiedy pętla może nie wykonać się ani razu
+        // do_while     - kiedy pętla musi wykonać się chociaż 1 raz
+
+        // [ -300, .. 5, 9, 1000 ]
+        //    ^^             ^^
+
+
+        // 1 1 1 1 1
+        int minWartość = 0;
+        int maxWartość = 0;
+        do {
+            // przyjmujemy liczby
+            int liczba = scanner.nextInt();
+
+            // jeśli liczba > 0
+            if (liczba > 0) {
+                //      zwiększ licznik
+                licznik++;
+            }
+
+            if (minWartość > liczba){
+                minWartość = liczba;
+            }
+
+            if (maxWartość < liczba) {
+                maxWartość = liczba;
+            }
+
+            // jeśli licznik >= 5
+            //      wyskocz z pętli
+        } while (licznik < 5);
+
+        System.out.println("Min: "+ minWartość);
+        System.out.println("Max: "+ maxWartość);
     }
 }
