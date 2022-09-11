14a79ce658a0dc7b5ef740d319954d5be0bb6f1c
Paweł Recław <pawel.reclaw@gmail.com>
2022-09-11 14:16:53 +0200

    Zadanie done

--- src/main/java/pl/sda/j133/sety/MainZadanie2.java
+++ src/main/java/pl/sda/j133/sety/MainZadanie2.java
@@ -1,5 +1,10 @@
 package pl.sda.j133.sety;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author Paweł Recław, AmeN
  * @project 133
@@ -13,12 +18,35 @@ package pl.sda.j133.sety;
 public class MainZadanie2 {
 
     private static boolean zawieraDuplikaty(String text) {
-        return false;
+        // "Ala ma kota"
+
+
+        // dodajemy do setu, wynik:
+        // [ 'A' 'l' 'a' ' ' 'm' 'k' 'o' 't' ]
+        char[] characters = text.toCharArray();
+
+        List<Character> list = new ArrayList<>();
+        for (char character : characters) {
+            list.add(character);
+        }
+        // [ 'A' 'l' 'a' ' ' 'm' 'a' ' ' 'k' 'o' 't' 'a' ]
+        //            ^           ^                   ^
+        // zawiera duplikaty: tak
+
+        Set<Character> set = new HashSet<>(list);
+
+        // [ 'A' 'l' 'a' ' ' 'm' 'a' ' ' 'k' 'o' 't' 'a' ]
+        // [ 'A' 'l' 'a' ' ' 'm' 'k' 'o' 't' ]
+
+//        if rozmiar jest taki sam => nie ma duplikatów
+        return set.size() != list.size();
+        // jeśli różnią się długością, to znaczy że jakaś litera
+        // została usunięta z setu jako duplikat, więc zawiera duplikaty = true
     }
 
     public static void main(String[] args) {
         System.out.println(zawieraDuplikaty("Ala ma kota"));    // true
         System.out.println(zawieraDuplikaty("kot"));            // false
-        System.out.println(zawieraDuplikaty("ale Kot_MA"));     // true
+        System.out.println(zawieraDuplikaty("ale Kot_MA"));     // false
     }
 }
